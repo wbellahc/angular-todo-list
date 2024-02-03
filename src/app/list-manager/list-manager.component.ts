8e10a9e8fc36f0925ca587b9416429940524be52
wbe <wbe@oto-technology.fr>
2024-02-03 15:34:08 +0100

    add create service

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputButtonUnitComponent } from '../input-button-unit/input-button-unit.component';
 import { TodoItemComponent } from '../todo-item/todo-item.component';
 import { TodoItem } from '../interfaces/todo-item';
+import { TodoListService } from '../services/todo-list.service';
 
 @Component({
   selector: 'todo-list-manager',
@@ -16,14 +17,11 @@ import { TodoItem } from '../interfaces/todo-item';
   styleUrl: './list-manager.component.scss'
 })
 export class ListManagerComponent {
-  todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
+  todoList: TodoItem[];
+
+  constructor(private todoListService: TodoListService) {
+    this.todoList = this.todoListService.getTodoList();
+  }
 
   addItem(title: string): void {
     this.todoList.push({ title: title });

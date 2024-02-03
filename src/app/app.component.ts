796271cb0b63f7973f470a91e7c092e9feaddf70
wbe <wbe@oto-technology.fr>
2024-02-03 11:13:43 +0100

    add input button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [InputButtonUnitComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todo-input-button-unit',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-button-unit.component.html',
+  styleUrl: './input-button-unit.component.scss'
+})
+
+export class InputButtonUnitComponent {
+  title = 'Learn about components';
+}
